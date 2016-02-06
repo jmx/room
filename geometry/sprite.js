9ed5162312bd913e1b6d84ce35baedead302a21d
James Macpherson <james.macpherson@salespreso.com>
2016-02-07 09:43:11 +1100

    feat(sprite): Flat geometry for temporary sprite

---
+++ geometry/sprite.js
@@ -0,0 +1,18 @@
+const positions = [
+	// top unique faces
+	[-0.5, 0.5, 1.0],
+	[0.5, 0.5, 1.0],
+	[0.5, -0.5, 1.0],
+	[-0.5, -0.5, 1.0]
+];
+
+const cells = [
+	// top face
+	[0, 2, 1],
+	[0, 3, 2]
+];
+
+export default {
+	positions,
+	cells
+}
