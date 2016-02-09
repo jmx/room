8ffe6bd97bb1c52fa2a63e4023529d5042169fc6
James Macpherson <james.macpherson@salespreso.com>
2016-02-10 08:06:07 +1100

    fix(textures): Use geo-3d-box to generate blocks
    Texture coordinates need (up to) 24 vertices on a cube to define all of the coordinates

--- geometry/box.js
+++ geometry/box.js
@@ -1,65 +1,70 @@
 import normals from "normals";
+import box from "geo-3d-box";
 
-const positions = [
-	// top unique faces
-	[-0.5, 0.5, 1.0],
-	[0.5, 0.5, 1.0],
-	[0.5, -0.5, 1.0],
-	[-0.5, -0.5, 1.0],
-	// bottom unique faces
-	[-0.5, 0.5, 0.0],
-	[0.5, 0.5, 0.0],
-	[0.5, -0.5, 0.0],
-	[-0.5, -0.5, 0.0]
-];
+export default box({size: 1, segments: 1});
 
-const cells = [
-	// top face
-	[0, 2, 1],
-	[0, 3, 2],
-	// south face
-	[3, 6, 2],
-	[3, 7, 6],
-	// east face
-	[2, 5, 1],
-	[2, 6, 5],
-	// west face
-	[0, 4, 7],
-	[0, 7, 3],
-	// north face
-	[0, 1, 5],
-	[0, 5, 4],
-];
+// const positions = [
+// 	// top unique faces
+// 	[-0.5, 0.5, 1.0],
+// 	[0.5, 0.5, 1.0],
+// 	[0.5, -0.5, 1.0],
+// 	[-0.5, -0.5, 1.0],
+// 	// bottom unique faces
+// 	[-0.5, 0.5, 0.0],
+// 	[0.5, 0.5, 0.0],
+// 	[0.5, -0.5, 0.0],
+// 	[-0.5, -0.5, 0.0]
+// ];
 
-// colors (sic) :-)
-const colors = [
-	[1.0, 0.0, 0.0],
-	[1.0, 0.0, 0.0],
-	[1.0, 0.0, 0.0],
-	[1.0, 0.0, 0.0],
-	[1.0, 0.0, 0.0],
-	[1.0, 0.0, 0.0],
-	[1.0, 0.0, 0.0],
-	[1.0, 0.0, 0.0]
-];
 
-const uvs = [
-	[0.5, 0.5],
-	[0.5, 1.0],
-	[1.0, 0.75],
-	[1.0, 0.25],
-	[0.5, 0.0],
-	[0.0, 0.25],
-	[0.0, 0.75],
-	[0.5, 0.0]
-];
+// const cells = [
+// 	// top face
+// 	[0, 2, 1],
+// 	[0, 3, 2],
+// 	// south face
+// 	[3, 6, 2],
+// 	[3, 7, 6],
+// 	// east face
+// 	[2, 5, 1],
+// 	[2, 6, 5],
+// 	// west face
+// 	[0, 4, 7],
+// 	[0, 7, 3],
+// 	// north face
+// 	[0, 1, 5],
+// 	[0, 5, 4],
+// ];
 
-const vertexNormals = normals.vertexNormals(cells, positions);
+// const uvs = [
+// 	[0.0, 1.0],
+// 	[1.0, 1.0],
+// 	[1.0, 0.0],
+// 	[0.0, 0.0],
+// 	[0.0, 0.0],
+// 	[0.5, 0.0],
+// 	[0.5, 0.5],
+// 	[0.0, 0.5],
+// ];
 
-export default {
-	positions,
-	cells,
-	colors,
-	uvs,
-	normals: vertexNormals
-}
+// // colors (sic) :-)
+// const colors = [
+// 	[1.0, 0.0, 0.0],
+// 	[1.0, 0.0, 0.0],
+// 	[1.0, 0.0, 0.0],
+// 	[1.0, 0.0, 0.0],
+// 	[1.0, 0.0, 0.0],
+// 	[1.0, 0.0, 0.0],
+// 	[1.0, 0.0, 0.0],
+// 	[1.0, 0.0, 0.0]
+// ];
+
+
+// const vertexNormals = normals.vertexNormals(cells, positions);
+
+// export default {
+// 	positions,
+// 	cells,
+// 	colors,
+// 	uvs,
+// 	normals: vertexNormals
+// }
